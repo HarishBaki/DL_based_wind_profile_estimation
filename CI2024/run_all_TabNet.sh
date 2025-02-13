4e9836e80a9715426fb2b1dc78b8d897aa88a691
HarishBaki <h.baki@tudelft.nl>
2025-02-13 21:50:46 +0100

    Remove obsolete files and scripts from the repository

--- CI2024/run_all_TabNet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# === multi output training ===#
-for j in $(seq 0 9); do
-python TabNet_multioutput.py "config.yaml" "[0,1,2,3,4]" "$j" &
-done
-echo "Done training"
--- data/rclone_copy_yearly.sh
+++
@@ -1,5 +0,0 @@
-source_dir=$1
-year=$2
-lev=$3
-ls $source_dir'/'$year'/'*'_'$lev'.grb' | xargs -I{} basename {} > 'files.txt'
-rclone copy --progress --transfers 1 --files-from 'files.txt' $source_dir'/'$year CERRA_complete/temp_dir/
